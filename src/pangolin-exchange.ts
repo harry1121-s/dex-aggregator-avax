1e8cc4e570101d5f078bef9e41e10f300318ab90
Harshit Singhal <82471455+harry1121-s@users.noreply.github.com>
2022-06-20 03:08:29 +0530

    DEX_AGG

--- src/pangolin-exchange.ts
+++ src/pangolin-exchange.ts
@@ -3,51 +3,80 @@ import {
   PangolinExchange,
   PairCreated
 } from "../generated/PangolinExchange/PangolinExchange"
-import { ExampleEntity } from "../generated/schema"
+import { Pair,Token } from "../generated/schema"
+import {
+  ERC20
+} from "../generated/PangolinExchange/ERC20"
+import { log } from '@graphprotocol/graph-ts'
 
-export function handlePairCreated(event: PairCreated): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handlePairCreatedPangolin(event: PairCreated): void {
+    // Get All Event Parameters
+    let _id = event.params.pair;
+    let _tokenA = event.params.token0;
+    let _tokenB = event.params.token1;
+    let _exchange = "PangolinExchange";
+    let _router = "";
+    let _pairId = event.params.param3;
+    log.debug("ID: {}\nTokenA: {}\nTokenB: {}\nPairID: {}", [_id.toHexString(),_tokenA.toHexString(),_tokenB.toHexString(),_pairId.toString()]);
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    // // Create SwapPair Entity for TokenA
+    // let swapPairA = SwapPair.load(_tokenA.toHexString());
+    // if(!swapPairA) {
+    //   swapPairA = new SwapPair(_tokenA.toHexString());
+    // }
+    // swapPairA.save();
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+    // // Create SwapPair Entity for TokenB
+    // let swapPairB = SwapPair.load(_tokenB.toHexString());
+    // if(!swapPairB) {
+    //   swapPairB = new SwapPair(_tokenB.toHexString());
+    // }
+    // swapPairB.save();
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+    // Create Token Instance A
+    let _erc20A = ERC20.bind(_tokenA);
+    let _nameA = _erc20A.name();
+    let _symbolA = _erc20A.symbol();
+    let _decimalA = _erc20A.decimals();
+    
+    // Create Token A instance
+    let tokenA =  Token.load(_tokenA.toHex());
+    if(tokenA === null) {
+      tokenA = new Token(_tokenA.toHex());
+      tokenA.name = _nameA;
+      tokenA.symbol = _symbolA;
+      tokenA.decimal = BigInt.fromI64(_decimalA);
+      // tokenA.tokenId = _tokenB.toHexString();
+    }
+    tokenA.save();
 
-  // Entity fields can be set based on event parameters
-  entity.token0 = event.params.token0
-  entity.token1 = event.params.token1
+    // Create Token Instance B
+    let _erc20B = ERC20.bind(_tokenB);
+    let _nameB = _erc20B.name();
+    let _symbolB = _erc20B.symbol();
+    let _decimalB = _erc20B.decimals();
+    
+    // Create Token A instance
+    let tokenB =  Token.load(_tokenB.toHex());
+    if(tokenB === null) {
+      tokenB = new Token(_tokenB.toHex());
+      tokenB.name = _nameB;
+      tokenB.symbol = _symbolB;
+      tokenB.decimal =  BigInt.fromI64(_decimalB);
+      // tokenB.tokenId = tokenA.id;
+    }
+    tokenB.save();
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.allPairs(...)
-  // - contract.allPairsLength(...)
-  // - contract.createPair(...)
-  // - contract.feeTo(...)
-  // - contract.feeToSetter(...)
-  // - contract.getPair(...)
+    // Create Token Pair
+    let pair = Pair.load(_id.toHex());
+    if(pair === null) {
+      pair = new Pair(_id.toHex());
+      pair.tokenA = _tokenA.toHexString();
+      pair.tokenAEntity = tokenA.id;
+      pair.tokenB = _tokenB.toHexString()
+      pair.tokenBEntity = tokenB.id;
+      pair.exchange = _exchange;
+      pair.router = _router;
+    }
+    pair.save();
 }
