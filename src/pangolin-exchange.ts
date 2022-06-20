3b9e5d0e247186788c094e98591497fbb86968a1
shreyas3336 <shreyasagarwal27@gmail.com>
2022-06-20 13:55:23 +0530

    Pangolin , Trader Joe

--- src/pangolin-exchange.ts
+++ src/pangolin-exchange.ts
@@ -14,10 +14,10 @@ export function handlePairCreatedPangolin(event: PairCreated): void {
     let _id = event.params.pair;
     let _tokenA = event.params.token0;
     let _tokenB = event.params.token1;
-    let _exchange = "PangolinExchange";
-    let _router = "";
+    let _exchange = "Pangolin Exchange";
+    let _router = "0xE54Ca86531e17Ef3616d22Ca28b0D458b6C89106";
     let _pairId = event.params.param3;
-    log.debug("ID: {}\nTokenA: {}\nTokenB: {}\nPairID: {}", [_id.toHexString(),_tokenA.toHexString(),_tokenB.toHexString(),_pairId.toString()]);
+    log.debug("PE - ID: {}\nTokenA: {}\nTokenB: {}\nPairID: {}", [_id.toHexString(),_tokenA.toHexString(),_tokenB.toHexString(),_pairId.toString()]);
 
     // // Create SwapPair Entity for TokenA
     // let swapPairA = SwapPair.load(_tokenA.toHexString());
@@ -44,17 +44,17 @@ export function handlePairCreatedPangolin(event: PairCreated): void {
     if(tokenA === null) {
       tokenA = new Token(_tokenA.toHex());
       if(_namecallA.reverted){
-        log.error("Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+        log.error("PE - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
       } else {
         tokenA.name = _namecallA.value;
       }
       if(_symbolcallA.reverted){
-        log.error("Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+        log.error("PE - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
       } else {
         tokenA.symbol = _symbolcallA.value;
       }
       if(_decimalcallA.reverted){
-        log.error("Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+        log.error("PE - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
       } else {
         tokenA.decimal = BigInt.fromI64(_decimalcallA.value);
       }
@@ -73,17 +73,17 @@ export function handlePairCreatedPangolin(event: PairCreated): void {
     if(tokenB === null) {
       tokenB = new Token(_tokenB.toHex());
       if(_namecallB.reverted){
-        log.error("Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+        log.error("PE - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
       } else {
         tokenB.name = _namecallB.value;
       }
       if(_symbolcallB.reverted){
-        log.error("Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+        log.error("PE - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
       } else {
         tokenB.symbol = _symbolcallB.value;
       }
       if(_decimalcallB.reverted){
-        log.error("Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+        log.error("PE - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
       } else {
         tokenB.decimal = BigInt.fromI64(_decimalcallB.value);
       }
@@ -103,4 +103,100 @@ export function handlePairCreatedPangolin(event: PairCreated): void {
       pair.router = _router;
     }
     pair.save();
+}
+
+export function handlePairCreatedJoe(event: PairCreated): void {
+  // Get All Event Parameters
+  let _id = event.params.pair;
+  let _tokenA = event.params.token0;
+  let _tokenB = event.params.token1;
+  let _exchange = "Joe Exchange";
+  let _router = "0x60aE616a2155Ee3d9A68541Ba4544862310933d4";
+  let _pairId = event.params.param3;
+  log.debug("TJ - ID: {}\nTokenA: {}\nTokenB: {}\nPairID: {}", [_id.toHexString(),_tokenA.toHexString(),_tokenB.toHexString(),_pairId.toString()]);
+
+  // // Create SwapPair Entity for TokenA
+  // let swapPairA = SwapPair.load(_tokenA.toHexString());
+  // if(!swapPairA) {
+  //   swapPairA = new SwapPair(_tokenA.toHexString());
+  // }
+  // swapPairA.save();
+
+  // // Create SwapPair Entity for TokenB
+  // let swapPairB = SwapPair.load(_tokenB.toHexString());
+  // if(!swapPairB) {
+  //   swapPairB = new SwapPair(_tokenB.toHexString());
+  // }
+  // swapPairB.save();
+
+  // Create Token Instance A
+  let _erc20A = ERC20.bind(_tokenA);
+  let _namecallA = _erc20A.try_name();
+  let _symbolcallA = _erc20A.try_symbol();
+  let _decimalcallA = _erc20A.try_decimals();
+  
+  // Create Token A instance
+  let tokenA =  Token.load(_tokenA.toHex());
+  if(tokenA === null) {
+    tokenA = new Token(_tokenA.toHex());
+    if(_namecallA.reverted){
+      log.error("TJ - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+    } else {
+      tokenA.name = _namecallA.value;
+    }
+    if(_symbolcallA.reverted){
+      log.error("TJ - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+    } else {
+      tokenA.symbol = _symbolcallA.value;
+    }
+    if(_decimalcallA.reverted){
+      log.error("TJ - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+    } else {
+      tokenA.decimal = BigInt.fromI64(_decimalcallA.value);
+    }
+    // tokenA.tokenId = _tokenB.toHexString();
+  }
+  tokenA.save();
+
+  // Create Token Instance B
+  let _erc20B = ERC20.bind(_tokenB);
+  let _namecallB = _erc20B.try_name();
+  let _symbolcallB = _erc20B.try_symbol();
+  let _decimalcallB = _erc20B.try_decimals();
+  
+  // Create Token A instance
+  let tokenB =  Token.load(_tokenB.toHex());
+  if(tokenB === null) {
+    tokenB = new Token(_tokenB.toHex());
+    if(_namecallB.reverted){
+      log.error("TJ - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+    } else {
+      tokenB.name = _namecallB.value;
+    }
+    if(_symbolcallB.reverted){
+      log.error("TJ - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+    } else {
+      tokenB.symbol = _symbolcallB.value;
+    }
+    if(_decimalcallB.reverted){
+      log.error("TJ - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+    } else {
+      tokenB.decimal = BigInt.fromI64(_decimalcallB.value);
+    }
+    // tokenB.tokenId = tokenA.id;
+  }
+  tokenB.save();
+
+  // Create Token Pair
+  let pair = Pair.load(_id.toHex());
+  if(pair === null) {
+    pair = new Pair(_id.toHex());
+    pair.tokenA = _tokenA.toHexString();
+    pair.tokenAEntity = tokenA.id;
+    pair.tokenB = _tokenB.toHexString()
+    pair.tokenBEntity = tokenB.id;
+    pair.exchange = _exchange;
+    pair.router = _router;
+  }
+  pair.save();
 }
